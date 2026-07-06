7dc69bef912227ab6610bd9baed0e7055e4e8550
Laotseu <67864504+LaoTseu0@users.noreply.github.com>
2026-07-06 19:47:22 +0200

    Fast fix: comment the VisiOverlay because cursor retrevial make a bug

--- home3d/src/App.jsx
+++ home3d/src/App.jsx
@@ -107,7 +107,7 @@ export default function App() {
       <EditBar />
       <InfoPanel />
       <VCBOverlay />
-      <VisitOverlay />
+      {/**<VisitOverlay />**/}
       <VisitSticks />
     </div>
   )
